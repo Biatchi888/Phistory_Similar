182baad177e183b5b5077a49329eb133c248b5a5
Biatchi888 <83083213+Biatchi888@users.noreply.github.com>
2023-10-15 01:16:25 +0800

    Initial commit

--- main/settings.py
+++ main/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 import os
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(4mldwfrd@+4hs(hw2h#g!l*fj()if&7qsu3v__dz0rwaakx=q"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -86,14 +84,21 @@ WSGI_APPLICATION = "main.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+# Load environment variables from the .env file
+from dotenv import load_dotenv
+
+load_dotenv()
+
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
-        'NAME': 'Product_Comparison_System',
-        'ENFORCE_SCHEMA': False,  # Set this to True to enforce a schema
+        'ENFORCE_SCHEMA': False,  # Set to True if you want to enforce schema
+        'CLIENT': {
+            'host': os.getenv('MONGODB_URI'),  # Use the environment variable
+        }
     }
 }
-# Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
