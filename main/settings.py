e19c2618ceaf6a02d6fe86d94cbd377f74586d9f
Biatchi888 <83083213+Biatchi888@users.noreply.github.com>
2023-10-15 01:34:07 +0800

    for deployement

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
